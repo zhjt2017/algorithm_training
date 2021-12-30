70f28d1366f9f4c7d41a82bceb688fe9acbcf2f8
bruce.zhu <bruce.zhu@snowballtech.com>
2021-12-30 19:55:33 +0800

    算法实现: 环形链表，给出环入口

--- src/com/speed/week1/CircularLinkedList.java
+++ src/com/speed/week1/CircularLinkedList.java
@@ -3,16 +3,21 @@ package com.speed.week1;
 import java.util.Arrays;
 
 /**
- * 环形链表-判断是否有环
+ * 环形链表
+ * -判断是否有环
+ * -如果有环, 给出环入口
  *
  * @author bruce.zhu@snowballtech.com
- * @since 2021-12-29 06:28:10
+ * @since 2021-12-30 06:37:01
  */
 public class CircularLinkedList {
     public static void main(String[] args) {
         // 方法1：直接使用HashSet, 时间复杂度O(n), 空间复杂度O(n), 这里就不赘言了
         // 方法2：双指针 (快慢相遇) (如果要计算环的长度, 2次相遇中间slow走过的长度, 这个就像是绕着操场长跑比赛一样)
-        System.out.println("hasCircle: " + hasCircle(input(new int[]{1, 2, 3, 4, 5, 6, 7, 4, 5})));
+        final int[] sourceIndexes = new int[]{1, 2, 3, 4, 5, 6, 7, 4, 5, 6};
+        System.out.println("hasCircle: " + hasCircle(input(sourceIndexes)));
+        // 进一步：计算环入口
+        System.out.println("circleInlet: " + circleInlet(input(sourceIndexes)));
     }
 
     /**
@@ -34,6 +39,36 @@ public class CircularLinkedList {
             }
         }
         return false;
+    }
+
+    /**
+     * 双指针方法找到环入口
+     *
+     * @param head
+     * @return 如果没有环, 返回null
+     */
+    private static LinkedNode circleInlet(final LinkedNode head) {
+        /*
+         * slow每个时间走一步, fast每个时间走2步
+         * 首次相遇时, 套了一圈, 继续走下去, 则相遇点不变, 则无结果
+         * 设环入口为第a个节点, slow到达入口后, 又走了b的距离与fast相遇, 而环剩余长度为c, 则a+b+n(b+c)=2*(a+b), a = (n-1)(b+c) + c
+         * 而当前位于b的位置, 即再走c就可以到达环入口, 然后一圈一圈等待, 只要二者已同样的速度, 一定可以在环入口处相遇
+         */
+        LinkedNode fast = head;
+        LinkedNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast.equals(slow)) {
+                fast = head;
+                while (!fast.equals(slow)) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
     }
 
     /**
