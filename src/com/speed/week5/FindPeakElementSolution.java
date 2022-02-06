10d254a4eece7398b31d91a47191d23ef73bdf85
bruce.zhu <bruce.zhu@snowballtech.com>
2022-02-06 17:44:05 +0800

    week5-基础学习: 二分拓展 - 寻找峰值

--- src/com/speed/week5/FindPeakElementSolution.java
+++ src/com/speed/week5/FindPeakElementSolution.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 算法实现: 寻找峰值
+ * - https://leetcode-cn.com/problems/find-peak-element/ (162题)
  * <p>
  * - 峰值元素是指其值严格大于左右相邻值的元素。
  * - 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
