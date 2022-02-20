af61fa3dce640befbc88ac32f6d35cde92e00883
bruce.zhu <bruce.zhu@snowballtech.com>
2022-02-20 21:46:11 +0800

    week-6：基础学习：贪心 - 完成所有任务的最少初始能量

--- src/com/teachingpractice/week6/MinInitialEnergySolution.java
+++ src/com/teachingpractice/week6/MinInitialEnergySolution.java
@@ -66,6 +66,16 @@ public class MinInitialEnergySolution {
         tasks = new int[][]{{1, 7}, {2, 8}, {3, 9}, {4, 10}, {5, 11}, {6, 12}};
         System.out.println("Input tasks : " + Arrays.deepToString(tasks));
         System.out.println("Output minimum initial energy : " + solution.minEffort(tasks));
+        System.out.println();
+
+        tasks = new int[][]{{1, 1}, {1, 4}, {1, 4}};
+        System.out.println("Input tasks : " + Arrays.deepToString(tasks));
+        System.out.println("Output minimum initial energy : " + solution.minEffort(tasks));
+        System.out.println();
+
+        tasks = new int[][]{{1, 2}, {2, 6}, {4, 8}};
+        System.out.println("Input tasks : " + Arrays.deepToString(tasks));
+        System.out.println("Output minimum initial energy : " + solution.minEffort(tasks));
     }
 
     /**
@@ -73,18 +83,24 @@ public class MinInitialEnergySolution {
      * - 自我定义：一个任务的额外能量 = 开始一个任务的最低能量 - 该任务实际耗费能量
      * - 但实际上，这是在需要最大的额外能量的任务最后一个执行时，我们的答案
      * - 如果先执行额外能量大的那些，那么就可以将额外能量在任务完成后用于其他任务：所以这个题本质还是：求一个顺序 (到底按什么顺序可以使得更多的额外能量可以更多地分摊到小的任务上)
-     * - 显然，按额外能量降序执行，即可实现 (这其实也就是贪心的思想，能够求得一种顺序)
+     * - 显然，按额外能量降序执行 (贪心策略)，即可实现总需要的初始能量最少 (此处我们是按照额外能量的这个含义来进行说明的，当然，这个还可以使用严谨的数学归纳法/邻项交换进行证明，这里略过，见文档笔记)
+     * - 至于这个初始能量到底是多少，我们反过来进行计算
+     * <p>
+     * - 时间复杂度 O(NlogN)
+     * - 空间复杂度 O(logN)
      *
      * @param tasks
      * @return
      */
     int minEffort(final int[][] tasks) {
-        int sum = 0;
-        int maxExtra = 0;
-        for (int[] task : tasks) {
-            sum += task[0];
-            maxExtra = Math.max(maxExtra, task[1] - task[0]);
+        // 按额外能量升序 (如果额外相同的，先计算任务最低开始能量小的) (这里我们只是写 (a, b) -> a[1] - a[0] - b[1] + b[0] 执行效果是一致的)
+        // (这一点是可以证明的：max(max(a[1], s + a[0]) + b[0], b[1]) 与 max(max(b[1], s + b[0]) + a[0], a[1]) 是相等的 (在a[1]=a[0]+k, b[1]=b[0]+k的条件下)
+        // max(max(a[0]+k, s + a[0]) + b[0], b[0]+k) == max(max(b[0]+k, s + b[0]) + a[0], a[0]+k)
+        Arrays.sort(tasks, (a, b) -> (a[1] - a[0] == b[1] - b[0] ? a[1] - b[1] : a[1] - a[0] - b[1] + b[0]));
+        int ans = 0;
+        for (final int[] task : tasks) {
+            ans = Math.max(task[1], ans + task[0]);
         }
-        return sum + maxExtra;
+        return ans;
     }
 }
